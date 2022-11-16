086b665745dca1c1b265ceb329efb4001a34139e
Tyler Reddy <tyler.je.reddy@gmail.com>
2022-11-16 11:16:03 -0700

    ENH: transfer granularity
    * `run_individual_benchmark()` now allows some
    "transfer granularity" in the sense that we can
    include or exclude HTD and DTH memory transfers in the
    CuPy timings (not yet for pykokkos though, which is harder
    to do...)
    
    * include the URL of the GitHub repo for this project
    in the default plot title
    
    * double the size of the 1D arrays used for benchmarking

--- perf_compare.py
+++ perf_compare.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import time
 from collections import defaultdict
 
@@ -11,10 +12,41 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 
-def run_individual_benchmark(func_obj, bench_data):
-    start = time.perf_counter()
+def run_individual_benchmark(func_obj, bench_data, include_mem_transfers: Optional[str] = None):
+    # we use include_mem_transfers mostly for CuPy at the moment, so that
+    # it pays the time cost for HTD/DTH transfer like PyKokkos
+    # does by default, to keep the performance comparisons
+    # fair
+    # TODO: how could we exclude the transfer timings
+    # for the pykokkos ufuncs? A bit more tricky than CuPy
+    # at the moment I think...
+    if include_mem_transfers == "cupy_include_transfers":
+        start = time.perf_counter()
+        bench_data = cp.asarray(bench_data)
+    elif include_mem_transfers == "cupy_exclude_transfers":
+        bench_data = cp.asarray(bench_data)
+        start = time.perf_counter()
+    else:
+        start = time.perf_counter()
+
     result = func_obj(bench_data)
+
+    if include_mem_transfers == "cupy_include_transfers":
+        result = result.get()
+    elif include_mem_transfers == "cupy_exclude_transfers":
+        # NOTE: cupyx.profiler.benchmark() for more granularity
+        # of CPU vs. GPU timings
+
+        # we synchronize the stream because there is otherwise some
+        # danger that the CUDA kernel is still running
+        # as far as the host is concerned
+        cp.cuda.stream.get_current_stream().synchronize()
+
     end = time.perf_counter()
+
+    if include_mem_transfers == "cupy_exclude_transfers":
+        result = result.get()
+
     elapsed_time_sec = end - start
     return result, elapsed_time_sec
 
@@ -25,12 +57,12 @@ def run_bench(array_size: int, num_trials: int = 3):
     """
     rng = np.random.default_rng(12345)
     data_np = np.float64(rng.random(array_size))
-    data_cp = cp.asarray(data_np)
     view = pk.View([array_size], dtype=pk.double)
     view[:] = data_np
     timing_data = {"numpy": defaultdict(list),
                    "pykokkos": defaultdict(list),
-                   "cupy": defaultdict(list)}
+                   "cupy_with_transfers": defaultdict(list),
+                   "cupy_no_transfers": defaultdict(list)}
     for func_name, pk_func, np_func, cp_func in tqdm([("cos", pk.cos, np.cos, cp.cos),
                                              ("sin", pk.sin, np.sin, cp.sin),
                                              ("tan", pk.tan, np.tan, cp.tan),
@@ -45,12 +77,15 @@ def run_bench(array_size: int, num_trials: int = 3):
         for trial in tqdm(range(num_trials)):
             result_numpy, time_numpy_sec = run_individual_benchmark(np_func, data_np)
             result_pykokkos, time_pykokkos_sec = run_individual_benchmark(pk_func, view)
-            result_cupy, time_cupy_sec = run_individual_benchmark(cp_func, data_cp)
+            result_cupy, time_cupy_sec = run_individual_benchmark(cp_func, data_np, "cupy_include_transfers")
+            result_cupy_no_xfer, time_cupy_sec_no_xfer = run_individual_benchmark(cp_func, data_np, "cupy_exclude_transfers")
             assert_allclose(result_pykokkos, result_numpy)
-            assert_allclose(result_cupy.get(), result_numpy)
+            assert_allclose(result_cupy, result_numpy)
+            assert_allclose(result_cupy_no_xfer, result_numpy)
             timing_data["numpy"][func_name].append(time_numpy_sec)
             timing_data["pykokkos"][func_name].append(time_pykokkos_sec)
-            timing_data["cupy"][func_name].append(time_cupy_sec)
+            timing_data["cupy_with_transfers"][func_name].append(time_cupy_sec)
+            timing_data["cupy_no_transfers"][func_name].append(time_cupy_sec_no_xfer)
 
     return timing_data, array_size
 
@@ -63,7 +98,8 @@ def plot_results(timing_data, array_size):
 
     for libname, offset in [("numpy", 0.0),
                             ("pykokkos", width),
-                            ("cupy", width * 2)]:
+                            ("cupy_with_transfers", width * 2),
+                            ("cupy_no_transfers", width * 3)]:
         avg_time = []
         std_time = []
         for func_name in x_labels:
@@ -85,7 +121,9 @@ def plot_results(timing_data, array_size):
     ax.set_xticklabels(x_labels)
     ax.set_xlabel("Function compared")
     ax.set_ylabel(f"Avg +- std dev of time (s) for {num_trials} trials [discard 1st trial]")
-    ax.set_title(f"Pykokkos unary ufunc performance vs. NumPy/CuPy for 1D array size of {array_size:.2E}",
+    ax.set_title((f"Pykokkos unary ufunc performance vs. NumPy/CuPy for 1D array size of {array_size:.2E}\n"
+                  "(benchmark source: https://github.com/tylerjereddy/pykokkos_perf_compare)"
+                 ),
                  fontsize=10)
     ax.legend()
     fig.savefig("pykokkos_vs_numpy_ufuncs.png",
@@ -94,7 +132,7 @@ def plot_results(timing_data, array_size):
 
 if __name__ == "__main__":
     pk.set_default_space(pk.ExecutionSpace.Cuda)
-    timing_data, array_size = run_bench(array_size=int(5e6),
+    timing_data, array_size = run_bench(array_size=int(1e7),
                                         num_trials=4)
     plot_results(timing_data=timing_data,
                  array_size=array_size)
